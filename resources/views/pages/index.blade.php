c6e0a1fb7f141451eff8a21c388a43c8edf361a8
Lance02345 <lancelevis19@gmail.com>
2024-02-20 10:24:23 +0300

    Homepage update

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,4 +1,201 @@
 @extends('layouts.agrodrop')
 @section('content')
-<p>hello</p>
+<body>
+
+<!-- Start Hero Section -->
+<div class="hero" style="background-image: url('images/HOME PAGE/Group 75.png');">
+    <div class="container" style =" font-color:white;">
+        <div class="row justify-content-between">
+            <div class="col-lg-5">
+                <div class="intro-excerpt">
+                    <h1>WE SERVE YOUR</h1>
+                    <h2 class="d-block">EVERY DRINKING WATER NEEDS</h2>
+                    <p class="mb-4">Your trusted source for pure and refreshing <br> drinking water.</p>
+                    <p><a href="#" class="btn btn-white-outline">Contact Us</a></p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- End Hero Section -->
+
+<!-- Start Product Section -->
+<div class="product-section">
+    <div class="container">
+    <div class="prdct" style="padding-bottom:30px; padding-top:0; padding-left:0; color: black;">
+        <h6>OUR PRODUCTS</h6>
+    </div>
+        <div class="row">
+
+            <!-- Start Column 1 -->
+            <div class="col-12 col-md-4 col-lg-3 mb-5 mb-md-0">
+                <a class="product-item" href="cart.html">
+                    <img src="images/HOME PAGE/20-LITRES-1 1.png" class="img-fluid product-thumbnail">
+                    <h3 class="product-title">20 Litres</h3>
+                    <h3 class="product-title">No Handle</h3>
+                    <h3 class="product-title">Bottle + Water</h3>
+                    <strong class="product-price">KES 1,300</strong>
+                </a>
+            </div> 
+            <!-- End Column 1 -->
+
+            <!-- Start Column 2 -->
+            <div class="col-12 col-md-4 col-lg-3 mb-5 mb-md-0">
+                <a class="product-item" href="cart.html">
+                    <img src="images/HOME PAGE/20-LITRES-1 1.png" class="img-fluid product-thumbnail">
+                    <h3 class="product-title">20 Litres</h3>
+                    <h3 class="product-title">No Handle</h3>
+                    <h3 class="product-title">Refill</h3>
+
+                    <strong class="product-price">KES 350</strong>
+                </a>
+            </div> 
+            <!-- End Column 2 -->
+
+            <!-- Start Column 3 -->
+            <div class="col-12 col-md-4 col-lg-3 mb-5 mb-md-0">
+                <a class="product-item" href="cart.html">
+                    <img src="images/HOME PAGE/Group 49.png" class="img-fluid product-thumbnail">
+                    <h3 class="product-title">20 Litres</h3>
+                    <h3 class="product-title">Inbuilt Handle</h3>
+                    <h3 class="product-title">Bottle + Water</h3>
+                    <strong class="product-price">KES 1,500</strong>
+                </a>
+            </div>
+            <!-- End Column 3 -->   
+
+            <!-- Start Column 4 -->
+            <div class="col-12 col-md-4 col-lg-3 mb-5 mb-md-0">
+                <a class="product-item" href="cart.html">
+                    <img src="images/HOME PAGE/Group 49.png" class="img-fluid product-thumbnail">
+                    <h3 class="product-title">20 Litres</h3>
+                    <h3 class="product-title">Inbuilt Handle</h3>
+                    <h3 class="product-title">Refill</h3>
+                    <strong class="product-price">KES 350</strong>
+                </a>
+            </div>
+            <!-- End Column 4 -->
+
+        </div>
+    </div>
+</div>
+<!-- End Product Section -->
+
+<!-- Start Why Choose 1tion -->
+<div class="accessories-section">
+    <div class="container">
+    <div class="prdct" style="padding-bottom:30px; padding-top:0; padding-left:0; color: black;">
+        <h6>ACCESSORIES</h6>
+    </div>
+        <div class="row justify-content-between">
+            <div class="col-md-4">
+                <img src="images/ACCESSORIES PAGE/Mask group (2).png" class="img-fluid" style="height: 400px; max-width:100%;">
+                <h6 style="text-align: left;">CUSTOM BOTTLES</h6>
+            </div>
+
+            <div class="col-md-4">
+                <img src="images/ACCESSORIES PAGE/Group 47.png" class="img-fluid" style="height: 400px; width:100%;">
+                <h6 style="text-align: center;">CUSTOM LABELS</h6>
+            </div>
+
+            <div class="col-md-4">
+                <img src="images/ACCESSORIES PAGE/1 1.png" class="img-fluid" style="height: 400px; max-width:100%;">
+                <h6 style="text-align: center;">BRANDED WEAR</h6>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- End Why Choose Us Section -->
+
+<div class="hero" style="background-image: url('images/HOME PAGE/Rectangle 15.png');">
+    <div class="container" style =" font-color:white;">
+        <div class="row justify-content-between">
+            <div class="col-md-2">
+                <div class="intro-excerpt">
+                    <h1>BACK TO WORK</h1>
+                    <h2 class="d-block">ESSENTIALS</h2>
+                    <p class="mb-4">Score everything you'll need for day one and beyond.</p>
+                    <p><a href="#" class="btn btn-white-outline">View</a></p>
+                </div>
+            </div>
+            <div class="col-md-2" style="padding: 0; margin: 0;">
+             <a href="#">
+              <img src="images/ACCESSORIES PAGE/Mask group (2).png" class="img-fluid" alt="Back to Work Essentials 1">
+             </a>
+            </div>
+           <div class="col-md-2" style="padding: 0; margin: 0;">
+             <a href="#">
+              <img src="images/ACCESSORIES PAGE/Mask group (3).png" class="img-fluid" alt="Back to Work Essentials 2">
+             </a>
+            </div>
+           <div class="col-md-2" style="padding: 0; margin: 0;">
+             <a href="#">
+              <img src="images/ACCESSORIES PAGE/1 1.png"class="img-fluid" alt="Back to Work Essentials 3">
+             </a>
+           </div>
+        </div>
+    </div>
+</div>
+
+<!-- End We Help Section -->
+
+
+<!-- Start Testimonial Slider -->
+<div class="testimonial-section">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-lg-12">
+                <div class="row">
+                    <div class="col-md-6">
+                        <!-- Text content goes here -->
+                        <div class="prdct" style="padding-bottom:30px; padding-top:0; padding-left:0; color: black;">
+                           <h6>ABOUT US</h6>
+                        </div>
+                        <p style="color: black;">Our journey began with a vital mission: to provide the purest and most refreshing drinking water to local families and communities. Established in 2022, we have since become a trusted name in the industry, committed to delivering not just water, but health and well being. We are a passionate and conscious company dedicated to providing you with the highest quality water. Ago drops water is filtered for purity, through an advanced treatment by the process of reverse osmosis and enhanced with minerals for refreshing and pure taste. We pride ourselves on our commitment to sustainability, using eco-friendly packaging.</p>
+                        <a href="#" class="btn btn-primary">Read More</a>
+                    </div>
+                    <div class="col-md-6">
+                        <!-- Photo goes here -->
+                        <img src="images/HOME PAGE/Mask Group (3).png" class="img-fluid" alt="Testimonial Photo">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- End Testimonial Slider -->
+
+<div class="testimonial-section">
+    <div class="container">
+    <div class="prdct" style="padding-bottom:30px; padding-top:0; padding-left:0; color: black;">
+                           <h6>FEATURED</h6>
+        </div>      
+        <div class="row justify-content-center">
+            <div class="col-lg-12">
+                <div class="row">
+                    <div class="col-md-6">
+                    <img src="images/HOME PAGE/Mask Group (4).png" class="img-fluid" alt="Testimonial Photo">
+                    </div>
+                    <div class="col-md-6">
+                        <!-- Photo goes here -->
+                        <img src="images/HOME PAGE/Mask Group (5).png" class="img-fluid" alt="Testimonial Photo">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="hero" style="background-image: url('images/HOME PAGE/Group 46.png');">
+    <div class="container">
+    </div>
+</div>
+
+
+<script src="{{ asset('js/bootstrap.bundle.min.js')}}"></script>
+<script src="{{ asset('js/tiny-slider.js')}}"></script>
+<script src="{{ asset('js/custom.js')}}"></script>
+</body>
 @endsection
